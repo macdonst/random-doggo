47ac953b901addd336454a7b0999137cf5d01445
Cole Peters <cole@colepeters.com>
2023-06-21 10:47:36 -0500

    Update styles

--- app/elements/dog-header.mjs
+++ app/elements/dog-header.mjs
@@ -1,7 +1,7 @@
 export default function DogHeader({ html }) {
   return html`
       <header class="pl0 pr0 pl5-lg pr5-lg">
-      <h1 class="text2 text4-lg text-center font-heading font-bold tracking-1 pt4 pt6-lg pb0 pb2-lg">
+      <h1 class="text2 text4-lg text-center font-serif font-bold tracking-1 pbs4 pbs6-lg pbe0 pbe2-lg">
         Random Doggo
       </h1>
     </header>
